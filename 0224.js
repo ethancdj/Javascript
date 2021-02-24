b7643bd0d3c7df2d311f55a2c649410e3e4d8d48
ethancdj <76196033+ethancdj@users.noreply.github.com>
2021-02-24 20:20:00 +0800

    Add files via upload

---
+++ 0224.js
@@ -0,0 +1,114 @@
+/** 
+const  janeOld = 14;
+const  janePhoneNumber = 0955713484;
+const  jsWakeUp = false;
+console.log(janeOld,janePhoneNumber,jsWakeUp);
+*/
+
+/** 
+const plainRicee= 50;
+const steak= 30;
+const drink= 10;
+let  walletTotal = 200;
+let totalPrice = walletTotal-((plainRicee*2)+(steak*2));
+console.log(`小明買完後，共剩下${totalPrice}元`);
+*/
+
+/** 
+//4-1
+const redLight = '紅燈';
+let seconds = 35;
+console.log(`小明抬頭看目前是${redLight}，剩餘秒數${seconds}秒`);
+
+//4-2
+let passersBy = 10;
+console.log(`馬路上目前${passersBy}個路人`);
+
+//4-3
+let cloud = 3;
+const sun = 1;
+console.log(`天空有${cloud}朵白雲以及${sun}個太陽`);
+*/
+
+/** 
+let integral = 50;
+let peopleCounting = 62;
+let day = 16 ;
+let hour = 9;
+console.log(`${integral}積分`);
+console.log(`${peopleCounting}人攻略成功`);
+console.log(`全體平均花費${day}天${hour}小時`);
+*/
+
+/** 
+let a = 1; //宣告一個a變數並賦予一個1的數字型別
+let b = 0; //宣告一個b變數並賦予一個0的數字型別
+a = 3;     //將數字型別寫入a的變數
+a = b + 2; //a變數的值為b+2=2
+a - b ;   //a變數-b變數 2-0=0
+b += 1;   //此寫法等於b=b+1 b=1
+*/
+
+/** 
+let maryAppleNum = 20;
+maryAppleNum -=5;
+maryAppleNum -=12;
+maryAppleNum +=4;
+console.log(`Mary還有${maryAppleNum}顆蘋果`);
+*/
+
+/** 
+let bill = 0;
+const hamburgerPrice = 30;
+const hamburgtotalPrice = 90;
+const milkteaPrice = 15;
+const drinkTotal = 32;
+bill = hamburgtotalPrice+(milkteaPrice*drinkTotal);
+console.log(`Nick的帳單總共${bill}元`);
+*/
+/** 
+let g = "hello"; //字串
+let h = 123; //數字
+let i = g+h; //字串
+let j = true; //布林值
+let k = h+h; //數字
+let l = j+k; //數字
+*/
+
+let group = '直播班第23組' ;
+let slackName = 'Ethan Chang';
+let profession = '工程師';
+let where = '台北';
+let interest =  '電影，電玩';
+let studyTime = '2-3';
+
+console,console.log(`大家好，${group}的 「${slackName}」，很高興認識各位! 我來自「${where}」，目前是一個「${profession}」，平常的興趣是「${interest}」。預計之後每天會花「${studyTime}」小時學習 JavaScript，未來還請多多指教。`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
